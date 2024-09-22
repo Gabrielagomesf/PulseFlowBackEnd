182bb1e067767e344e3f952ffd3b0e2518ebeb83
Gabrielagomesf <goomes.016@gmail.com>
2024-09-22 18:30:45 -0300

    registro/login funioanando e registrando no bd

--- server.js
+++ server.js
@@ -1,20 +1,52 @@
-const { MongoClient } = require('mongodb');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/auth');
+const profileRoutes = require('./routes/profile');
+const passwordResetRoutes = require('./routes/passwordReset');
 
-// URL de conexão com o MongoDB
-const uri = "url_da_conexão_mongoo";
+const app = express();
 
-const client = new MongoClient(uri);
+app.use(bodyParser.json());
 
-async function connectToDatabase() {
-  try {
-    await client.connect();
+// Conexão ao MongoDB
+const uri = "url_do_bd";
 
-    console.log("Conectado com sucesso ao MongoDB!");
-    const database = client.db('PULSEFLOW')
+mongoose.connect(uri, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+})
+.then(() => console.log('Conectado ao MongoDB'))
+.catch(err => {
+    console.error('Erro ao conectar ao MongoDB:', err.message);
+    process.exit(1);
+});
 
-  } catch (error) {
-    console.error("Erro ao conectar ao MongoDB: ", error);
-  }
-}
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
 
-connectToDatabase();
+// Rotas
+app.use('/api/auth', authRoutes);
+app.use('/api/profile', profileRoutes);
+app.use('/api/password-reset', passwordResetRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Servidor está rodando!');
+});
+
+app.use((req, res, next) => {
+    res.status(404).send('Rota não encontrada');
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Ocorreu um erro no servidor');
+});
+
+// Inicializa o servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
